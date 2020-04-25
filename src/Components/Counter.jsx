5d75c9368a1c42ed32c4ef0fa0280c8f6152c47a
Julien Martos <ju_sxm@hotmail.fr>
2020-04-25 16:47:07 +0200

    add a input to write a number

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+const Counter = () => {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+  const handleChange = event => {
+    setCount(Number(event.target.value));
+  };
+  return (
+    <div>
+      <p>Le compteur est à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <input
+        type="number"
+        placeholder="enter a number"
+        onChange={handleChange}
+      ></input>
+    </div>
+  );
+};
+
+export default Counter;
